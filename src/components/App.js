373deabb8435d0b3cba9d3d127193fbe6695e7c0
nick8604 <97799306+nick8604@users.noreply.github.com>
2022-04-24 18:45:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name"> My Own Website </h1>
+    <p data-ns-test="project-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deleniti quos id? Quos ipsa nesciunt voluptate! Deleniti aut aliquam quasi vitae laboriosam necessitatibus non eos? </p>
+    </div>
   )
 }
 
